1ec3236affcd80e325df1a506e3b82aaeeda1f53
Luis Angel Ortega <angel-ortega12@hotmail.com>
2021-10-06 16:14:37 -0600

    Added config to deploy to Github Pages

--- doczrc.js
+++ doczrc.js
@@ -1,5 +1,7 @@
 export default {
   title: "Luis Angel Ortega",
   description: "Hello, I'm Luis Angel and this is my CV made with Docz!",
-  ignore: ["README.md"]
+  ignore: ["README.md"],
+  dest: "/docs",
+	base: "docz-resume"
 }
